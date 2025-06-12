ac18eb6954ef66f0833ffdb6e981ea7128be285f
정재영 <wodud04277@gmail.com>
2025-06-12 15:32:59 +0900

    [Feat] Ball Return Place, Rename Bowling Ball Table to SnackBar Table
    볼링공이 되돌아오는 곳을 만듦, 원래 있던 Ball Table은 볼링장같지 않아서 나중에 쓸 스낵바 테이블로 socket과 함계 재사용 예정.

---
+++ Assets/Scripts/SpawnBallButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBallButton : MonoBehaviour
+{
+    public void SpawnBowlingBall()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            
+        }
+    }
+}
