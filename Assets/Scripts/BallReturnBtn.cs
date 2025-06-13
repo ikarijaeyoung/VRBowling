4244ad644ba842ee8c171441b17149ee2a0be03c
정재영 <wodud04277@gmail.com>
2025-06-13 10:50:18 +0900

    [Feat] Return Ball Button Click Sound
    소리를 나게하여, 사용자가 버튼을 눌렀을 때 상호작용이 잘 되고있음을 알려줌.

--- Assets/Scripts/BallReturnBtn.cs
+++ Assets/Scripts/BallReturnBtn.cs
@@ -6,8 +6,19 @@ public class ReturnBallButton : MonoBehaviour
 {
     [SerializeField] GameObject _ballPrefab;
     [SerializeField] Transform _ballReturnPoint;
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] AudioClip _audioClip;
+    void Start()
+    {
+        if (_audioSource == null) Debug.Log("Audio Source가져오지 못함.");
+    }
     public void ReturnBall()
     {
+        if (_audioSource != null) _audioSource.PlayOneShot(_audioClip);
+        else Debug.Log("Audio Source가 없음");
+        Debug.Log("Audio 재생");
+        
         Instantiate(_ballPrefab, _ballReturnPoint.position, _ballReturnPoint.rotation);
+        Debug.Log("공 생성");
     }
 }
