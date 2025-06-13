170772a4d0ecd8ca47e430398946936b1f515a14
정재영 <wodud04277@gmail.com>
2025-06-13 14:44:30 +0900

    [Feat] Bowling Pins Reset Button
    Hover했을 때, 사용법이 적힌 UI가 보여짐.
    버튼을 누르면 핀의 위치 재설정.

--- Assets/Scripts/ResetPinBtn.cs
+++ Assets/Scripts/ResetPinBtn.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class ResetPinBtn : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] List<GameObject> bowlingPins;
+    private Dictionary<GameObject, Vector3> initPinPositions = new Dictionary<GameObject, Vector3>();
+    private Dictionary<GameObject, Quaternion> initPinRotations = new Dictionary<GameObject, Quaternion>();
+    [SerializeField] Canvas canvas;
     void Start()
     {
-        
+        foreach (GameObject pin in bowlingPins)
+        {
+            if (pin != null)
+            {
+                initPinPositions[pin] = pin.transform.position;
+                initPinRotations[pin] = pin.transform.rotation;
+            }
+            else
+            {
+                Debug.Log("Init error : Pin이 할당되지 않음.");
+            }
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ResetBowlingPins()
     {
-        
+        foreach (GameObject pin in bowlingPins)
+        {
+            if (pin != null)
+            {
+                pin.transform.position = initPinPositions[pin];
+                pin.transform.rotation = initPinRotations[pin];
+
+                Rigidbody rb = pin.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.velocity = Vector3.zero;
+                    rb.angularVelocity = Vector3.zero;
+                }
+            }
+            else
+            {
+                Debug.Log("Reset error : Pin이 할당되지 않음.");
+            }
+        }
+        Debug.Log("Bowling Pins Reset Success");
     }
 }
